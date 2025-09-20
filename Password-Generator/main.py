26607c10c3b42b831cc5aadc7b68a9a2a6bb476f
Chengiz Khan <0xmoeedkhan@gmail.com>
2025-09-20 17:25:41 +0500

    Password Generator Updated

---
+++ Password-Generator/main.py
@@ -0,0 +1,41 @@
+#Password Generator
+
+#importing modules, strings and random
+
+import string
+import random
+
+#using input for length
+
+length = int(input("How many Characters You want in your password?: "))
+
+print('''Select Password Critiera from below:
+1. Digits
+2. Letters
+3. Special Characters
+4. Exit ''')
+
+CharacterList = ""
+
+while True:
+    select = int(input("Pick a number from above: "))
+
+    if select == 1:
+        CharacterList += string.digits
+    elif select == 2:
+        CharacterList += string.ascii_letters
+    elif select == 3:
+        CharacterList += string.punctuation
+    elif select == 4:
+        break
+    else:
+        print("Invalid Input")
+        continue
+
+password = []
+
+for i in range (length):
+    randomCharacter = random.choice(CharacterList)
+    password.append(randomCharacter)
+
+print("The Password is: "+"".join(password))
